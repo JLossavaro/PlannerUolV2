b4ad0c1e3736fab40d798c30049877f3f8fe6f48
GiovaneIwamoto <giovaneiwamoto@gmail.com>
2023-03-09 16:00:25 -0400

    chore: nodemon added

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 const route = Router()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.listen(3333, () => 'server running on port 3333')
+app.listen(3333, () => console.log('server running on port 3333'))
 const baseRoute = '/api/v1';
 app.use(baseRoute, UsersRoute);
 app.use(baseRoute, EventsRoute);
