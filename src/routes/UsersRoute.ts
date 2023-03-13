c36e6def89e5660424c67007b4280db9155478a8
JLossavaro <jclossavaro10@gmail.com>
2023-03-13 15:16:46 -0300

    feature: put/remove-swagger-implement

--- src/routes/UsersRoute.ts
+++ src/routes/UsersRoute.ts
@@ -11,6 +11,12 @@ const userController = new UserController();
  * tags:
  *   name: Events
  *   description: All operations with events.
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
  */
 
 /**
@@ -113,11 +119,89 @@ route.post('/users/signIn', (req: Request, res: Response) => {
   const user = userController.login(req, res);
 });
 
+
+/**
+ * @swagger
+ * /users/updateUser:
+ *   put:
+ *     summary: Update your own User
+ *     tags:
+ *       - Users
+ *     security:
+ *     - bearerAuth: []
+ *     requestBody:
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 description: Primeiro nome do usuário
+ *               lastName:
+ *                 type: string
+ *                 description: Último nome do usuário
+ *               birthDate:
+ *                 type: string
+ *                 description: Data de nascimento do usuário
+ *               city:
+ *                 type: string
+ *                 description: Cidade do usuário
+ *               country:
+ *                 type: string
+ *                 description: País do usuário
+ *               email:
+ *                 type: string
+ *                 description: Endereço de e-mail do usuário
+ *               password:
+ *                 type: string
+ *                 description: Senha do usuário
+ *             required:
+ *               - email
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado com sucesso
+ *       400:
+ *         description: Requisição inválida (algum campo obrigatório não foi preenchido corretamente)
+ *       401:
+ *         description: Não autorizado (o usuário não tem permissão para acessar este recurso)
+ *       500:
+ *         description: Erro interno do servidor
+ */
 route.put('/users/updateUser', authMiddleware, (req: Request, res: Response) => {
   const user = userController.updateUser(req, res);
 });
 
-
+/**
+ * @swagger
+ *
+ * /users/deleteUser:
+ *   delete:
+ *     summary: delete your own user.
+ *     tags: 
+ *       - Users
+ *     security:
+ *     - bearerAuth: []
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email of your user.
+ *             required:
+ *               - email
+ *     responses:
+ *       200:
+ *         description: The user was logged successfully.
+ *       400:
+ *         description: Invalid request body or parameters.
+ */
 route.delete('/users/deleteUser', authMiddleware, (req: Request, res: Response) => {
   const user = userController.deleteUser(req, res);
 });
